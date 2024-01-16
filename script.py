c45f87f92bbb6bd9451d326704464945c33513aa
Suheil Nofal <suheil.nofal619@gmail.com>
2024-01-16 14:29:50 +0100

    Second assignment

--- script.py
+++ script.py
@@ -28,14 +28,24 @@ for month in months:
                 monthly_precipitation += input['value']
     total_monthly_precipitation.append(monthly_precipitation)
 
-print(total_monthly_precipitation)
+#calculating the yearly precipitation using the list of monthly precipitation:
+total_yearly_precipitation = 0
+for month in total_monthly_precipitation:
+    total_yearly_precipitation += month
+
+#calculating the relative monthly precipitation:
+relative_mothly_precipitation = []
+for month in total_monthly_precipitation:
+    relative_mothly_precipitation.append(month/total_yearly_precipitation) 
 
 #this creates a json file with the output as specified below
 output = {
      "Seattle": {
           "station": code,
           "state": "WA",
-          "total_monthly_precipitation": total_monthly_precipitation
+          "total_monthly_precipitation": total_monthly_precipitation,
+          "total_yearly_precipitation": total_yearly_precipitation,
+          "relative_monthly_precipitation": relative_mothly_precipitation
      }
 }
 
